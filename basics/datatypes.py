044322742053968fb48b1a1b66ae635388231def
Alexander Potashnik <sashapotash@Alexanders-MBP.fios-router.home>
2018-06-19 22:50:49 -0400

    start

---
+++ basics/datatypes.py
@@ -0,0 +1,33 @@
+# Basic Data Types
+
+# NOTE Python is not strongly typed. Lots of interpretation so
+# be sure to give variable names that mean something.
+
+number = 10
+
+string = "Hello World"
+
+floating = 1.00786
+
+words = ["oneword","another","and another"]
+
+numbers = [1,9,30,109892.20,109]
+# You can mix and match data types in arrays
+# Any basic Data type.
+
+dictionary = {"key":"value","anotherkey":100,10:"number!"}
+# Key Value Pair data structure. Think
+# given a word i can get the definition
+
+# Math
+
+sum1 = 20 + 10
+# print(sum1)
+
+product = 10*89
+# print(product)
+
+quotient = 9/3
+# print(quotient)
+
+
